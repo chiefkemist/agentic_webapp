fcdb8e9d5896764a4c1ff534a5f27685ca5206ac
chiefkemist <chiefkemist+git@outcastgeek.tech>
2024-08-09 01:59:37 -0400

    Basic Streaming UI

--- src/agentic_webapp/dmbr/tools.py
+++ src/agentic_webapp/dmbr/tools.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+
+import operator
+from typing import Any
+from langchain_core.tools import tool
+
+
+@tool("add")
+def add(a: Any, b: Any) -> Any:
+    """
+    Addition: Add two numbers together
+    """
+    return operator.add(a, b)
+
+
+@tool("sub")
+def sub(a: Any, b: Any) -> Any:
+    """
+    Substraction: Substract one number from the other
+    """
+    return operator.sub(a, b)
+
+
+@tool("mul")
+def mul(a: Any, b: Any) -> Any:
+    """
+    Multiplication: Multiply two numbers
+    """
+    return operator.mul(a, b)
+
+
+@tool("truediv")
+def truediv(a: Any, b: Any) -> Any:
+    """
+    Division: Divide one number by the other
+    """
+    return operator.truediv(a, b)
